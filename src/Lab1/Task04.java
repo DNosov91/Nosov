e6d90465632a0affaf25fa94c554f5985d31a975
DNosov91 <be.a.true.target@gmail.com>
2017-11-27 19:39:16 +0600

    Добавлены файлы в проект

--- src/Lab1/Task04.java
+++ src/Lab1/Task04.java
@@ -6,5 +6,5 @@ public class Task04 {public static void main(String[] args) {
     Scanner s=new Scanner(System.in);
     String a;
     a=s.nextLine();
-    System.out.println(a+" нас не остановят!");
-}
+    System.out.println("Программирование это "+a+"!");
+}}
